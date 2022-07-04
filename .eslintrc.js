ff802dd4c84c5a615c4dd8bfee15c0926cb30019
Pavel Lisitsyn <pavellisicyn69@gmail.com>
2022-07-04 17:38:07 +0300

    init project

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: ["plugin:react/recommended", "standard"],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    plugins: ["react"],
+    rules: {
+        semi: [2, "always"],
+        quotes: [
+            "error",
+            "double",
+            { allowTemplateLiterals: true, avoidEscape: true },
+        ],
+        "comma-dangle": ["error", "only-multiline"],
+        "space-before-function-paren": ["error", { named: "never" }],
+        "multiline-ternary": ["off"],
+        indent: ["error", 4, { SwitchCase: 1 }],
+    },
+};
---
+++ src/app/app.jsx
@@ -0,0 +1,5 @@
+const App = () => {
+    return "Money manger";
+};
+
+export default App;
