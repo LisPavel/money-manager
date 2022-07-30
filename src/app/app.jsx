6b208c2c6e20be327768fb3f3d3624788ca702e2
Pavel Lisitsyn <pavellisitsyn69@gmail.com>
2022-07-30 17:37:57 +0300

    added nav bar

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -2,12 +2,13 @@ import React from "react";
 // import TestLayout from "./layouts/testLayout";
 // import Transactions from "./layouts/transactions";
 import { Switch, Route, Redirect } from "react-router-dom";
+import NavBar from "./components/ui/navBar";
 import appRoutes from "./routes/app.routes";
 
 const App = () => {
     return (
         <div>
-            <h1>Money Manager</h1>
+            <NavBar routes={appRoutes} title={"My Money Manager"} />
             <Switch>
                 {Object.keys(appRoutes).map((route) => {
                     return <Route {...appRoutes[route]} key={route} />;
